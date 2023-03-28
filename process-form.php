dde4fc8edb13687a067f1aedeb81b56906c1bbd1
kbtrack94 <128067558+kbtrack94@users.noreply.github.com>
2023-03-27 20:32:35 -0400

    Create process-form.php

---
+++ process-form.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // Process and store the form data here, such as inserting it into a database or sending it via email.
+}
+?>
