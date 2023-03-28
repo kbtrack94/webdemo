b999f88a4d52d11f55a39491926200f93a8f82cc
kbtrack94 <128067558+kbtrack94@users.noreply.github.com>
2023-03-27 20:52:10 -0400

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -5,5 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message = $_POST["message"];
 
   // Process and store the form data here, such as inserting it into a database or sending it via email.
+
+  // Open the CSV file for writing
+  $file = fopen("form_data.csv", "a");
+
+  // Write the form data to the file
+  fputcsv($file, array($name, $email, $message));
+
+  // Close the file
+  fclose($file);
 }
 ?>
